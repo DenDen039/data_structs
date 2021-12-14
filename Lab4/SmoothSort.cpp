41f2187e5a80953d487767a880248fa547401f73
Denis Denysyev <dvdenysyev@gmail.com>
2021-12-14 13:43:44 +0200

    Lab 4 begin

--- Lab4/SmoothSort.cpp
+++ Lab4/SmoothSort.cpp
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+void SmoothSort(int *arr, int size){ 
+    
+}
 int main()
 {
-
+    srand(time(0));
+    system("clear");
+    int size = 100;
+    int *arr = new int[size];
+    for(int i = 0; i < size;i++)
+        arr[i] = rand()%size+1;
+    SmoothSort(arr,size);
+    for(int i = 0; i < size;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }

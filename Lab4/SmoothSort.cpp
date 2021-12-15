0814b64aa00b16c2ba8bdf0033e23087b21a86e4
Denis Denysyev <dvdenysyev@gmail.com>
2021-12-15 21:47:05 +0200

    Finished lab 4

--- Lab4/SmoothSort.cpp
+++ Lab4/SmoothSort.cpp
@@ -1,6 +1,5 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
 int *LeonardoNums(int size)
 {
     int limit_size = size;
@@ -26,90 +25,106 @@ int *LeonardoNums(int size)
     }
     return nums;
 }
-void RestoreHeap(int heap_size, int *heap, int i, int *arr, int arr_size, int *leo_nums)
+void RestoreHeap(int *arr, int arr_size, int *heap,int heap_size, int * leonardo_nums, int i )
 {
-    int current = heap_size - 1;
-    int k = heap[current];
-    while (current > 0)
+    int current_heap = heap_size-1;
+    
+    while(current_heap > 0)
     {
-        int j = i - leo_nums[k];
-        if (arr[j] > arr[i] &&
-            (k < 2 || arr[j] > arr[i - 1] && arr[j] > arr[i - 2]))
-        {
-            swap(arr[j], arr[i]);
-            i = j;
-            current--;
-            k = heap[current];
-        }
-        else
+        if (i < leonardo_nums[heap[current_heap]]) {
             break;
-    }
-    while (k >= 2)
-    {
-        int t_r = i - 1, k_r = k - 2;
-        int t_l = t_r - leo_nums[k_r], k_l = k - 1;
-        if (arr[i] < arr[t_r] || arr[i] < arr[t_l])
-            if (arr[t_r] > arr[t_l])
+        }
+        int j = i-leonardo_nums[heap[current_heap]];
+        if(arr[j] > arr[i])
+        {
+            if(i-j > 1)
             {
-                swap(arr[i], arr[t_r]);
-                i = t_r;
-                k = k_r;
+                int right = i-1;
+                int left = i-1-leonardo_nums[heap[current_heap]-2];
+                if (arr[j] > arr[left] && arr[j] > arr[right])
+                    swap(arr[i],arr[j]);
+                else 
+                    break;
             }
+            else 
+                swap(arr[i],arr[j]);
+                    
+        }
+        else 
+            break;
+        current_heap--;
+        i = j;
+    }
+
+    int cur_heap_size = heap[current_heap];
+    while  (cur_heap_size>= 2){
+        int right_child = i - 1, right_size_index  = cur_heap_size - 2;
+        int left_child = i-1-leonardo_nums[cur_heap_size-2], left_size_index = cur_heap_size - 1;
+        if (arr[i] < arr[right_child] || arr[i] < arr[left_child]){
+            if (arr[right_child] > arr[left_child])
+            {
+                swap(arr[i], arr[right_child]);
+                i = right_child;
+                cur_heap_size = right_size_index;
+            }  
             else
             {
-                swap(arr[i], arr[t_l]);
-                i = t_l;
-                k = k_l;
-            }
+                swap(arr[i], arr[left_child]);
+                i = left_child;
+                cur_heap_size = left_size_index;
+            } 
+        }
         else
             break;
     }
 }
 void SmoothSort(int *arr, int size)
 {
-    int *leonardo_nums = LeonardoNums(size);
-    int *heap = new int[2 * size];
-    int cur_heap = 0;
+    int *leonadro_nums =LeonardoNums(size);
+    int *heap = new int [size],cur_heap_size = 0;
 
-    for (int i = 0; i < size; i++)
-    {
-        if (cur_heap >= 2 && heap[cur_heap - 1] + 1 == heap[cur_heap - 2])
+    for(int i = 0; i < size; i++){
+        if(cur_heap_size >= 2 && heap[cur_heap_size-1]+1== heap[cur_heap_size-2])
         {
-            cur_heap--;
-            heap[cur_heap - 1]++;
+            cur_heap_size--;
+            heap[cur_heap_size-1]++;
         }
-        else
-        {
-            if (cur_heap >= 1 && heap[cur_heap - 1] == 1)
-            {
-                heap[cur_heap++] = 0;
-            }
+        else{
+            if(cur_heap_size >= 1 && heap[cur_heap_size-1] == 1)
+                heap[cur_heap_size++] = 0;
             else
-                heap[cur_heap++] = 1;
+                heap[cur_heap_size++] = 1;
         }
-        RestoreHeap(cur_heap, heap, i, arr, size, leonardo_nums);
+        RestoreHeap(arr,size,heap,cur_heap_size,leonadro_nums,i);
     }
+
     for (int i = size - 1; i >= 0; i--)
     {
-        if (heap[cur_heap - 1] < 2)
-            cur_heap--;
+        if (heap[cur_heap_size - 1] < 2)
+            cur_heap_size--;
         else
         {
-            int k = heap[--cur_heap];
-            int t_r = i - 1, k_r = k - 2;
-            int t_l = t_r - leonardo_nums[k_r], k_l = k - 1;
-            heap[cur_heap++] = k_l;
-            RestoreHeap(cur_heap, heap, t_l, arr, size, leonardo_nums);
-            heap[cur_heap++] = k_r;
-            RestoreHeap(cur_heap, heap, t_r, arr, size, leonardo_nums);
+            int cur_size = heap[cur_heap_size-1];
+            cur_heap_size--;
+            int right_child = i - 1, right_size_index  = cur_size - 2;
+            int left_child = i-1-leonadro_nums[cur_size-2], left_size_index = cur_size - 1;
+
+            heap[cur_heap_size++] = left_size_index;
+            RestoreHeap(arr,size,heap,cur_heap_size,leonadro_nums,left_child);
+
+            heap[cur_heap_size++] = right_size_index;
+            RestoreHeap(arr,size,heap,cur_heap_size,leonadro_nums,right_child);
         }
     }
+
+    delete leonadro_nums;
+    delete heap;
 }
-int main()
+int main ()
 {
     srand(time(0));
     system("clear");
-    int size = 20;
+    int size = 5;
     int *arr = new int[size];
     for (int i = 0; i < size; i++)
         arr[i] = rand() % size + 1;
@@ -126,3 +141,4 @@ int main()
     cout << endl;
     return 0;
 }
+            
